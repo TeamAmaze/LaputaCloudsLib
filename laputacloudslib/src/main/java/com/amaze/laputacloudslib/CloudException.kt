7f6d1b1342cb30c5513b251fd80b7f903cf6a84d
Emmanuel M <emmanuelbendavid@gmail.com>
2020-02-24 11:13:49 -0300

    DropBox: Upload files

--- laputacloudslib/src/main/java/com/amaze/laputacloudslib/CloudException.kt
+++ laputacloudslib/src/main/java/com/amaze/laputacloudslib/CloudException.kt
@@ -1,6 +1,21 @@
 package com.amaze.laputacloudslib
 
+import com.dropbox.core.DbxException
 import java.io.IOException
 
-abstract class CloudException(e: Exception) : IOException(e)
+abstract class CloudException : IOException {
+    constructor(e: Exception): super(e)
+    constructor(message: String): super(message)
+    constructor(message: String, e: Exception): super(message, e)
+}
 
+open class DropBoxIOException: CloudException {
+    constructor(e: DbxException): super(e)
+    constructor(message: String): super(message)
+    constructor(message: String, e: DbxException): super(message, e)
+}
+
+class TooManyFailsException : DropBoxIOException {
+    constructor(message: String) : super(message)
+    constructor(message: String, lastException: DbxException) : super(message, lastException)
+}
