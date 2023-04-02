2202439b407fc9cbd84889bb73cef84002b935b1
EmmanuelMess <emmanuelbendavid@gmail.com>
2023-04-02 12:42:49 -0300

    Fix unit tests

--- laputacloudslib/src/test/java/com/amaze/laputacloudslib/AbstractFileStructureTest.kt
+++ laputacloudslib/src/test/java/com/amaze/laputacloudslib/AbstractFileStructureTest.kt
@@ -6,19 +6,9 @@ import org.junit.Assert.*
 
 class AbstractFileStructureTest {
     @Test
-    fun test_AbstractFileStructure_removeScheme() {
-        assertEquals("/fs/..",
-            AbstractFileStructureDriver.removeScheme(
-                "a:/fs/..",
-                "a:"
-            )
-        )
-    }
-
-    @Test
     fun test_AbstractFileStructure_sanitizeRawPath() {
         assertEquals("/",
-            AbstractFileStructureDriver.sanitizeRawPath(
+            AbstractCloudPath.sanitizeRawPath(
                 "a/b/../../"
             )
         )
